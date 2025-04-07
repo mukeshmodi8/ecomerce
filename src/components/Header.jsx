96a92adac02b0f448929143abc3d204f418e5054
mukeshmodi8 <mvmodi.2014@gmail.com>
2025-04-07 11:20:00 +0530

    Updated project and fixed 404 error issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,18 +44,23 @@ const Header = () => {
           </button>
         </div>
 
-        <div className="user-actions">
-          <NavLink to="/wishlist" className="icon wishlist">
-            <FaHeart /> <span>Wishlist</span>
-          </NavLink>
-          <NavLink to="/cart" className="icon cart">
-            <FaShoppingCart /> <span>Cart</span>
-            <span className="cart-count">0</span>
-          </NavLink>
-          <NavLink to="/login" className="icon signin">
-            <FaUser /> <span>Sign In</span>
-          </NavLink>
-        </div>
+        <div className="user-actions d-flex gap-2">
+  <NavLink to="/wishlist" className="btn btn-outline-primary d-flex align-items-center gap-2">
+    <FaHeart /> <span>Wishlist</span>
+  </NavLink>
+
+  <NavLink to="/cart" className="btn btn-outline-success d-flex align-items-center gap-2 position-relative">
+    <FaShoppingCart /> <span>Cart</span>
+    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger cart-count">
+      0
+    </span>
+  </NavLink>
+
+  <NavLink to="/login" className="btn btn-outline-dark d-flex align-items-center gap-2">
+    <FaUser /> <span>Sign In</span>
+  </NavLink>
+</div>
+
 
         <button
           className="menu-icon"
@@ -66,18 +71,17 @@ const Header = () => {
       </div>
 
       <div className={`header-bottom ${dropdownOpen ? "open" : ""}`}>
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/computer">Computers & Laptop</NavLink>
-        <NavLink to="/tablets">Tablets</NavLink>
-        <NavLink to="/drones">Drones & Cameras</NavLink>
-        <NavLink to="/audio">Audio</NavLink>
-        <NavLink to="/mobile">Mobile</NavLink>
-        <NavLink to="/tv">T.V & Home Cinema</NavLink>
-        <NavLink to="/wearable-tech">Wearable Tech</NavLink>
-        <NavLink to="/sale" className="sale">
-          Sale
-        </NavLink>
-      </div>
+  <NavLink to="/home">Home</NavLink>
+  <NavLink to="/computer">Computers & Laptop</NavLink>
+  <NavLink to="/tablets">Tablets</NavLink>
+  <NavLink to="/drones">Drones & Cameras</NavLink>
+  <NavLink to="/audio">Audio</NavLink>
+  <NavLink to="/mobile">Mobile</NavLink>
+  <NavLink to="/tv">T.V & Home Cinema</NavLink>
+  <NavLink to="/wearable-tech">Wearable Tech</NavLink>
+  <NavLink to="/sale" className="sale">Sale</NavLink>
+</div>
+
     </nav>
   );
 };
